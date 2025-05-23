779d0d2ae87e58a9b005246b03bf931f5b0e3846
Hzb <hazeebville@gmail.com>
2025-05-23 20:17:51 +0100

    binarysearch

--- Searchalgorithms/Binarysch.java
+++ Searchalgorithms/Binarysch.java
@@ -1,5 +1,32 @@
 package Searchalgorithms;
 
 public class Binarysch {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 0; i < 99; i++) {
+            array[i] = i;
+        }
+        int target = 68;
+        int result = binarysearch(array, target);
+        System.out.println("Vlaue is at index " + result);
+        
+    }
     
+    public static int binarysearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int value = array[middle];
+
+            if (value < target)
+                left = middle + 1;
+            else if (value > target)
+                right = middle - 1;
+            else
+                return middle;
+        }
+        return -1;
+    }
 }
